35d6e652bffdaf9a756f7f8dd99551a67ceaa989
stupidkubik <stupidkubik@gmail.com>
2022-06-01 19:37:17 +0300

    add function to pass all test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+const repeatString = (string, num) => {
+    let newString = '';
+    if (num < 0) {
+        return newString = "ERROR";
+    } else for (; num > 0; num--) {
+        newString += string; 
+    }
+    return newString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
