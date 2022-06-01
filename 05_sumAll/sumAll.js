17f319ee90c5edc32a8c29f395ab81d5d95ba654
stupidkubik <stupidkubik@gmail.com>
2022-06-01 21:14:35 +0300

    add function to pass all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (typeof x === "number" && typeof y === "number") {
+        if (x > y && y > 0) {
+            for (;y <= x; y++) {
+                sum += y;
+            }
+            return sum;
+        } else if (x < y && x > 0) {
+            for (;x <= y; x++) {
+                sum += x;
+            }
+            return sum;
+        } else return sum = 'ERROR';
+    } else return sum = 'ERROR';
 };
 
 // Do not edit below this line
