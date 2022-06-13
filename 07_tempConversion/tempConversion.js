2b3a00012cc7b3eb24afb141b08d9f7b49034c02
stupidkubik <stupidkubik@gmail.com>
2022-06-13 17:18:34 +0300

    add functions to pass all test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let newTemperature = (temperature - 32) * 5 / 9;
+  if (newTemperature < 0) {
+    return newTemperature = -(Math.round(Math.abs(newTemperature) * 10) / 10);
+  } else {
+    return newTemperature = Math.round(newTemperature * 10) / 10;
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let newTemperature = temperature * 9 / 5 + 32;
+  if (newTemperature < 0) {
+    return newTemperature = -(Math.round(Math.abs(newTemperature) * 10) / 10);
+  } else {
+    return newTemperature = Math.round(newTemperature * 10) / 10;
+  }
 };
 
 // Do not edit below this line
