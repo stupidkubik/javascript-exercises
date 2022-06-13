2fb2f89adb8ea9735f8fc04bebc8ea71448d00e6
stupidkubik <stupidkubik@gmail.com>
2022-06-13 16:47:01 +0300

    add function to pass all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(yeah) {
+    const fourYears = yeah % 4;
+    const oneCentury = yeah % 100;
+    const fourCentury = yeah % 400;
 
+    if (fourCentury === 0) {
+        return true; 
+    } else if (oneCentury === 0) {
+        return false;
+    } else if (fourYears === 0) {
+        return true;
+    } else return false;
 };
 
 // Do not edit below this line
