d47754646a235bbb3f6f1db6fe1b3c40469fc57b
stupidkubik <stupidkubik@gmail.com>
2022-06-13 22:26:13 +0300

    add function to pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (palString) {
+    let newPalString1 = palString.replaceAll(/[^\w]/g, "").toLowerCase();
+    let newPalString2 = newPalString1.split("").reverse().join("");
 
-};
+    if (newPalString1 === newPalString2) {
+        return true;
+    } else return false;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
