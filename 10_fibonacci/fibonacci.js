d85fb0f1ec23c385a9ee26f01783cd05229954d6
stupidkubik <stupidkubik@gmail.com>
2022-06-14 20:58:56 +0300

    add function to pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number <= 0) {
+        return "OOPS";
+    } else {
+        let fib = [1, 1];
+        for (let i = 2; i < 30; i++) {
+            fib.push(fib[i - 2] + fib[i - 1]);
+        }
+        return fib[number - 1];
+    }
 };
 
 // Do not edit below this line
